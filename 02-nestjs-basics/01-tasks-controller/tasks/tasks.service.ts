3e5684cd33b8d5a3a38c856ac2eb06232a08bbc7
Viacheslav Stepanian <v.stepanyan@magnus-tech.ru>
2025-06-01 13:07:40 +0400

    Создание контроллера для CRUD операций с задачами

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -3,15 +3,42 @@ import { Task } from "./task.model";
 
 @Injectable()
 export class TasksService {
+  private taskIdCounter = 1;
+
+  generateIdForNewTask(): string {
+    return String(this.taskIdCounter++);
+  }
+
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const task = this.tasks.find(({ id: taskId }) => taskId === id);
+    if (!task) throw new NotFoundException(`Task with id ${id} not found`);
+    return task;
+  } 
 
-  createTask(task: Task): Task {}
+  createTask(task: Omit<Task, 'id'>): Task {
+    const newTask = {
+      ...task,
+      id: this.generateIdForNewTask(),
+    };
+    this.tasks.push(newTask);
+    return newTask;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  updateTask(id: string, update: Partial<Omit<Task, 'id'>>): Task {
+    const task = this.getTaskById(id);
+    Object.keys(update).forEach((key) => { task[key] = update[key] });
+    return task;
+  }
 
-  deleteTask(id: string): Task {}
+  deleteTask(id: string): Task {
+    const task = this.getTaskById(id);
+    this.tasks = this.tasks.filter(({ id: taskId }) => id !== taskId);
+    return task;
+  }
 }
